0c61319bb2b42cf037d82507fba195d7cc215fc6
awolferret <104518827+awolferret@users.noreply.github.com>
2022-06-14 18:47:04 +0300

    1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            DataBase dataBase = new DataBase();
+        }
+    }
+
+    class DataBase
+    {
+        private List<Criminal> _criminals = new List<Criminal> { new Criminal("Hulio", false, 185, 85, "Hispanic"), new Criminal("Julio", false, 180, 75, "Hispanic"), new Criminal("John", true, 175, 95, "American"), new Criminal("Ivan", false, 185, 85, "Russian") };
+        private List<Criminal> _filtredCriminals;
+
+        public DataBase()
+        {
+            _filtredCriminals = FilterArrested();
+            Search();
+        }
+
+        public void Search()
+        {
+            bool isWorking = true;
+
+            while (isWorking)
+            {
+                Console.WriteLine("1. Поиск по имени");
+                Console.WriteLine("2. Поиск по росту");
+                Console.WriteLine("3. Поиск по весу");
+                Console.WriteLine("4. Поиск по национальности");
+                Console.WriteLine("5. Выход");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        NameSearch();
+                        break;
+                    case "2":
+                        HeightSearch();
+                        break;
+                    case "3":
+                        WeightSearch();
+                        break;
+                    case "4":
+                        NationalitySearch();
+                        break;
+                    case "5":
+                        isWorking = false;
+                        break;
+                    default:
+                        Console.WriteLine("Ошибка");
+                        break;
+                }
+            }
+        }
+
+        private void NameSearch()
+        {
+            Console.WriteLine("Введите имя");
+            string input = Console.ReadLine();
+            var nameFilred = from Criminal criminal in _filtredCriminals where criminal.Name == (input) select criminal;
+
+            foreach (var criminal in nameFilred)
+            {
+                Console.WriteLine(criminal.Name + " " + criminal.Height + " " + criminal.Weight + " " + criminal.Nationality);
+            }
+        }
+
+        private void HeightSearch()
+        {
+            Console.WriteLine("Введите рост");
+            string input = Console.ReadLine();
+            int number;
+
+            if (int.TryParse(input, out number))
+            {
+                var heightFilred = from Criminal criminal in _filtredCriminals where criminal.Height == (number) select criminal;
+
+                foreach (var criminal in heightFilred)
+                {
+                    Console.WriteLine(criminal.Name + " " + criminal.Height + " " + criminal.Weight + " " + criminal.Nationality);
+                }
+            }
+        }
+
+        private void WeightSearch()
+        {
+            Console.WriteLine("Введите вес");
+            string input = Console.ReadLine();
+            int number;
+
+            if (int.TryParse(input, out number))
+            {
+                var weightFilred = from Criminal criminal in _filtredCriminals where criminal.Weight == (number) select criminal;
+
+                foreach (var criminal in weightFilred)
+                {
+                    Console.WriteLine(criminal.Name + " " + criminal.Height + " " + criminal.Weight + " " + criminal.Nationality);
+                }
+            }
+        }
+
+        private void NationalitySearch()
+        {
+            Console.WriteLine("Введите национальность");
+            string input = Console.ReadLine();
+            var nationalityFilred = from Criminal criminal in _filtredCriminals where criminal.Nationality == (input) select criminal;
+
+            foreach (var criminal in nationalityFilred)
+            {
+                Console.WriteLine(criminal.Name + " " + criminal.Height + " " + criminal.Weight + " " + criminal.Nationality);
+            }
+        }
+
+        private List<Criminal> FilterArrested()
+        {
+            var filtredCriminals = from Criminal criminal in _criminals where criminal.IsArrested == false select criminal;
+            return filtredCriminals.ToList();
+        }
+    }
+
+    class Criminal
+    { 
+        public string Name { get; private set; }
+        public bool IsArrested { get; private set; }
+        public int Height { get; private set; }
+        public int Weight { get; private set; }
+        public string Nationality { get; private set; }
+
+        public Criminal(string name, bool isArrested, int height, int weight, string nationality)
+        {
+            Name = name;
+            IsArrested = isArrested;
+            Height = height;
+            Weight = weight;
+            Nationality = nationality;
+        }
+    }
+}
